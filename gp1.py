0a771fb2e8b62bee6b74e8b4490f04c579b57521
srivish12 <nalincha30@.gmail.com>
2025-07-18 14:55:39 +0100

    created a code to calcutate number of patients admitted per month.

--- gp1.py
+++ gp1.py
@@ -2,14 +2,15 @@ from datetime import datetime
 
 
 class Patient:
-    def __init__(self, patient_id, name, age, gender, diagnosis, dateOfAdmit):
+    def __init__(self, patient_id, name, age, gender, diagnosis,
+                 date_of_admission):
         self.patient_id = patient_id
         self.name = name
         self.age = age
         self.gender = gender
         self.diagnosis = diagnosis
-        self.date_of_admission = dateOfAdmit
-
+        self.date_of_admission = date_of_admission
+        
     def __str__(self):
         return (
             f"ID: {self.patient_id}, Name: {self.name}, Age: {self.age}, "
@@ -19,16 +20,16 @@ class Patient:
 
 
 """
-# Patient registration
+* Patient registration
 
-# Viewing all patients
+* Viewing all patients
 
-# Searching patients by ID
+* Searching patients by ID
 
-# Updating patient records
+* Updating patient records
 
-# Deleting records#
-# number of patient admitted and discharged to predict future trends
+* Deleting records
+* number of patient admitted and discharged to predict future trends
 
  """
 
@@ -75,7 +76,7 @@ class GP_Practice:
             if name:
                 patient.name = name
             if age:
-                patient.age = age
+                patient.age = int(age)
             if gender:
                 patient.gender = gender
             if diagnosis:
@@ -91,7 +92,7 @@ class GP_Practice:
         else:
             print("Patient ID not found.")
 
-    def get_gender_input():
+    def get_gender_input(self,):
         gender = input("Gender (male/female): ").strip().lower()
         if gender in ["male", "m"]:
             return "male"
@@ -100,88 +101,116 @@ class GP_Practice:
         else:
             return "invalid value"
 
-    def main():
-        gppractice = GP_Practice()
 
-        while True:
-            print("\n---Patient Management System in GP ---")
-            print("1. Add Patient")
-            print("2. View All Patients")
-            print("3. Search Patient by ID")
-            print("4. Search Patient by Name")
-            print("5. Update Patient")
-            print("6. Delete Patient")
-            print("7. Number Of Patients Admitted")
-            print("8. Exit")
+def main():
+    gppractice = GP_Practice()
 
-            choice = input("Enter the Number which you want to look: ")
+    while True:
+        print("\n---Patient Management System in GP ---")
+        print("1. Add Patient")
+        print("2. View All Patients")
+        print("3. Search Patient by ID")
+        print("4. Search Patient by Name")
+        print("5. Update Patient")
+        print("6. Delete Patient")
+        print("7. Number Of Patients Admitted")
+        print("8. Number Of Patients Admitted per Day")
+        print("9. Number Of Patients Admitted per Month")
+        print("10. Exit")
 
-            if choice == "1":
-                pid = input("Patient ID: ")
-                name = input("Name: ")
-                age = int(input("Age: "))
-                gender = gppractice.get_gender_input()
-                if gender == "invalid value":
-                    print("Invalid gender, please enter male or female")
+        choice = input("Enter the Number which you want to look: ")
+
+        if choice == "1":
+            pid = input("Patient ID: ")
+            name = input("Name: ")
+            age = int(input("Age: "))
+            gender = gppractice.get_gender_input()
+            if gender == "invalid value":
+                print("Invalid gender, please enter male or female")
+            else:
+                diagnosis = input("Diagnosis: ")
+                date_input = input("Date of Admission (YYYY-MM-DD):")
+
+                try:
+                    date_of_admission = datetime.strptime(
+                        date_input, "%Y-%m-%d").date()
+
+                except ValueError:
+                    print("Invalid date format. Please use YYYY-MM-DD.")
+                patient = Patient(pid, name, age, gender, diagnosis,
+                                  date_of_admission)
+                gppractice.add_patient(patient)
+
+        elif choice == "2":
+            gppractice.view_all_patients()
+
+        elif choice == "3":
+            pid = input("Enter Patient ID: ")
+            gppractice.search_patient_by_id(pid)
+
+        elif choice == "4":
+            name = input("Enter Patient Name: ")
+            gppractice.search_patient_by_name(name)
+
+        elif choice == "5":
+            pid = input("Enter Patient ID to update: ")
+            print("Leave field blank if no change is needed.")
+            name = input("New Name: ")
+            age = input("New Age: ")
+            gender = input("New Gender: ")
+            diagnosis = input("New Diagnosis: ")
+            gppractice.update_patient(
+                pid,
+                name or None,
+                age or None,
+                gender or None,
+                diagnosis or None
+            )
+
+        elif choice == "6":
+            pid = input("Enter Patient ID to delete: ")
+            gppractice.delete_patient(pid)
+
+        elif choice == "7":
+            gppractice.view_count = len(gppractice.patients)
+            print(f"Total Number of patients Registered : "
+                  f"{gppractice.view_count}")
+
+        elif choice == "8":
+            patient_counts = {}
+
+            for patient in gppractice.patients.values():
+                date = patient.date_of_admission
+                if date in patient_counts:
+                    patient_counts[date] += 1
                 else:
-                    diagnosis = input("Diagnosis: ")
-                    date_input = input("Date of Admission (YYYY-MM-DD):")
-                    try:
-                        date_Of_admission = datetime.strptime(
-                            date_input, "%Y-%m-%d"
-                        ).date()
-                    except ValueError:
-                        print("Invalid date format. Please use YYYY-MM-DD.")
-                        continue
-                    patient = Patient(pid, name, age, gender, diagnosis,
-                                      date_Of_admission)
-                    gppractice.add_patient(patient)
+                    patient_counts[date] = 1
 
-            elif choice == "2":
-                gppractice.view_all_patients()
+            print("Number of patients admitted per day:")
+            for date in patient_counts:
+                print(f"{date}: {patient_counts[date]}")
 
-            elif choice == "3":
-                pid = input("Enter Patient ID: ")
-                gppractice.search_patient_by_id(pid)
+        elif choice == "9":
+            patient_counts = {}
 
-            elif choice == "4":
-                name = input("Enter Patient Name: ")
-                gppractice.search_patient_by_name(name)
+            for patient in gppractice.patients.values():
+                month = patient.date_of_admission.strftime("%Y-%m")
+                if month in patient_counts:
+                    patient_counts[month] += 1
+                else:
+                    patient_counts[month] = 1
 
-            elif choice == "5":
-                pid = input("Enter Patient ID to update: ")
-                print("Leave field blank if no change is needed.")
-                name = input("New Name: ")
-                age = input("New Age: ")
-                gender = input("New Gender: ")
-                diagnosis = input("New Diagnosis: ")
-                gppractice.update_patient(
-                    pid,
-                    name or None,
-                    age or None,
-                    gender or None,
-                    diagnosis or None
-                )
+            print("Number of patients admitted per month:")
+            for month in patient_counts:
+                print(f"{month}: {patient_counts[month]}")
 
-            elif choice == "6":
-                pid = input("Enter Patient ID to delete: ")
-                gppractice.delete_patient(pid)
+        elif choice == "10":
+            print("Exiting the Program!")
+            break
 
-            elif choice == "7":
-                gppractice.view_count = len(gppractice.patients)
-                print(f"Total Number of patients Registered : "
-                      f"{gppractice.view_count}")
+        else:
+            print("Invalid choice. Try again.")
 
-            elif choice == "8":
-                patient_counts = {}
 
-                for patient in gppractice.patients.values():
-                    date = patient.date_of_admission
-                    if date in patient_counts:
-                        patient_counts[date] += 1
-                    else:
-                        patient_counts[date] = 1
-
-                print("Number of patients admitted per day:")
-                for date in patient_counts:
-                    print(f"{date}: {patient_counts[date]}")
+if __name__ == "__main__":
+    main()
